c5704d64d40d0f882a9d57a4225ed4584b6ea3af
giobruno <giobruno@me.com>
2022-09-27 15:38:50 +0200

    done jsx 2

---
+++ jsx-02/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+var name = 'Giovanni';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Hello {name} </h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
